5ed873325bb61ad26381a5c00464425f58f3a607
petraalpigiano <petraalpigiano@gmail.com>
2025-05-12 17:26:06 +0200

    add static images

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Il mio server sta ascoltando correttamente alla porta: ${port}`);
 });
