91fe71ebdcb22815fd577ff762fdbfd760628e18
petraalpigiano <petraalpigiano@gmail.com>
2025-05-12 17:03:43 +0200

    add array

---
+++ posts.js
@@ -0,0 +1,33 @@
+const posts = [
+  {
+    title: "Ciambellone",
+    content: "Ricetta del ciambellone",
+    img: "/images/ciambellone.jpeg",
+    tags: ["Dolce", "Vaniglia"],
+  },
+  {
+    title: "Cracker alla barbabietola",
+    content: "Ricetta dei cracker alla barbabietola ",
+    img: "/images/cracker_barbabietola.jpeg",
+    tags: ["Salato", "Barbabietola"],
+  },
+  {
+    title: "Pane fritto dolce",
+    content: "Ricetta del pane fritto dolce ",
+    img: "/images/pane_fritto_dolce.jpeg",
+    tags: ["Dolce", "Pane"],
+  },
+  {
+    title: "Pasta alle barbabietole",
+    content: "Ricetta della pasta alle barbabietole ",
+    img: "/images/pasta_barbabietola.jpeg",
+    tags: ["Salato", "Pasta"],
+  },
+  {
+    title: "Torta paesana",
+    content: "Ricetta della torta paesana",
+    img: "/images/torta_paesana.jpeg",
+    tags: ["Dolce", "Torta"],
+  },
+];
+export default posts;
